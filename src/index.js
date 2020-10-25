b682f30eacab793389ee31709ab7f5d80009a05b
RaspAcid <rasp.acid@gmail.com>
2020-10-25 12:58:32 +0300

    feat: implement towel-sort task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,41 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let arr = matrix;
+  if(Array.isArray(arr)){
+  for(let i = 0; i<arr.length; i++){
+    if(i & 1){
+    for(let j = 0; j<arr[i].length; j++){
+      for(let k = 0; k<arr[i].length; k++){
+        if(arr[i][k]<arr[i][k+1]){
+          var temp = arr[i][k+1]; 
+          arr[i][k+1] = arr[i][k]; 
+          arr[i][k] = temp;
+          }
+        }
+      }
+    }else{
+      for(let j = 0; j<arr[i].length; j++){
+        for(let k = 0; k<arr[i].length-i; k++){
+          if(arr[i][k]>arr[i][k+1]){
+            var temp = arr[i][k+1]; 
+            arr[i][k+1] = arr[i][k]; 
+            arr[i][k] = temp;
+            }
+          }
+        }
+      }
+    }
+
+    let arr2 = [];
+    for (let subArr of arr) {
+	    for (let elem of subArr) {
+		  arr2.push(elem);
+	    }
+    }
+
+    return arr2;
+  }else{
+    return [];
+  }
 }
